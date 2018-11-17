1a0ba7ab5c4dcdf431936823c96b93d7053062ae
alexie <ualex73@gmail.com>
2018-11-17 21:21:23 +0100

    Added: arm32 and arm64 support. Changed: Moved to Alpine image.

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VERSION=$1
+
+if [ ! -z "$VERSION" ]; then
+  VERSION="-${VERSION}"
+fi
+
+for ARCH in amd64  arm32v6 arm64v8
+do
+  docker build -f Dockerfile.${ARCH} -t ualex73/dsmr-reader-docker:${ARCH} .
+
+  # Tag it with a version
+  if [ ! -z "$VERSION" ]; then
+    docker tag ualex73/dsmr-reader-docker:${ARCH} ualex73/dsmr-reader-docker:${ARCH}${VERSION}
+  fi
+done
+
+if [ ! -z "$VERSION" ]; then
+  docker tag ualex73/dsmr-reader-docker:amd64 ualex73/dsmr-reader-docker:latest
+  docker tag ualex73/dsmr-reader-docker:amd64 ualex73/dsmr-reader-docker:$VERSION
+fi
+
