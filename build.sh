7b8149666511469d6e73a62f912f2bede8767a8d
alexie <ualex73@gmail.com>
2018-12-23 17:35:56 +0100

    Updated to version 1.27.0 Added arm32v6 and arm64v8 support

--- build.sh
+++ build.sh
@@ -2,17 +2,13 @@
 
 VERSION=$1
 
-if [ ! -z "$VERSION" ]; then
-  VERSION="-${VERSION}"
-fi
-
 for ARCH in amd64  arm32v6 arm64v8
 do
   docker build -f Dockerfile.${ARCH} -t ualex73/dsmr-reader-docker:${ARCH} .
 
   # Tag it with a version
   if [ ! -z "$VERSION" ]; then
-    docker tag ualex73/dsmr-reader-docker:${ARCH} ualex73/dsmr-reader-docker:${ARCH}${VERSION}
+    docker tag ualex73/dsmr-reader-docker:${ARCH} ualex73/dsmr-reader-docker:${ARCH}-${VERSION}
   fi
 done
 
